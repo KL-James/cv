2fe0a70d467e7e2d6babd0b0b1a8e757b84710dd
Gjw <47529550+KL-James@users.noreply.github.com>
2019-07-24 19:28:21 +0800

    Update message.js

--- js/message.js
+++ js/message.js
@@ -32,9 +32,9 @@
                 )
         },
         bindEvents: function(){    
-            this.form.addEventListener('submit', function(e){
+            this.form.addEventListener('submit', (e) => {
                 e.preventDefault()
-                this.savemessage()
+                this.saveMessage()
             })
         },
         saveMessage: function(){
